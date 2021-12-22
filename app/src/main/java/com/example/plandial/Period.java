c41beef414079b7a0d1572d6667bb6df22fcd32e
CaChiJ <chj7239@gmail.com>
2021-12-23 00:57:09 +0900

    Period 클래스 및 테스트 코드

---
+++ app/src/main/java/com/example/plandial/Period.java
@@ -0,0 +1,33 @@
+package com.example.plandial;
+
+public class Period {
+    private final UnitOfTime unitOfTime;
+    private final int times;  // '시간'이 아닌 '배'의 의미임. 즉, period = times * unitOfTime
+
+    private Period(UnitOfTime unitOfTime, int times) {
+        this.unitOfTime = unitOfTime;
+        this.times = times;
+    }
+
+    public static Period createPeriodOrNull(UnitOfTime unitOfTime, int times) {
+        /*
+         * 다음의 경우에 유효하지 않은 객체 생성으로 판단하고 null을 반환함
+         * 1) times가 0보다 작은 경우
+         * 2) getPeriodInMillis 함수 호출 시 오버플로우가 발생하는 경우
+         */
+
+        if (times <= 0 || unitOfTime.getSeconds() > Long.MAX_VALUE / (times * 1000)) {
+            return null;
+        }
+
+        return new Period(unitOfTime, times);
+    }
+
+    public long getPeriodInSeconds() {
+        return unitOfTime.getSeconds() * times;
+    }
+
+    public long getPeriodInMillis() {
+        return unitOfTime.getSeconds() * UnitOfTime.MILLIS_PER_SECOND * times;
+    }
+}
---
+++ app/src/test/java/com/example/plandial/PeriodTest.java
@@ -0,0 +1,90 @@
+package com.example.plandial;
+
+import org.junit.Test;
+
+public class PeriodTest {
+    @Test
+    public void When_MakePeriodValid_Expect_ReturnValidInstance() {
+        Period period;
+
+        period = Period.createPeriodOrNull(UnitOfTime.HOUR, 1);
+        assert period != null;
+        assert period.getPeriodInSeconds() == 3600;
+        assert period.getPeriodInMillis() == 3600000;
+
+        period = Period.createPeriodOrNull(UnitOfTime.HOUR, 12);
+        assert period != null;
+        assert period.getPeriodInSeconds() == 43200;
+        assert period.getPeriodInMillis() == 43200000;
+
+        period = Period.createPeriodOrNull(UnitOfTime.HOUR, 24);
+        assert period != null;
+        assert period.getPeriodInSeconds() == 86400;
+        assert period.getPeriodInMillis() == 86400000;
+
+        period = Period.createPeriodOrNull(UnitOfTime.HOUR, 100);
+        assert period != null;
+        assert period.getPeriodInSeconds() == 360000;
+        assert period.getPeriodInMillis() == 360000000;
+    }
+
+
+    @Test
+    public void When_MakeTooLongPeriod_Expect_ReturnNull() {
+        Period period;
+
+        period = Period.createPeriodOrNull(UnitOfTime.HOUR, Integer.MAX_VALUE);
+        assert period == null;
+
+        period = Period.createPeriodOrNull(UnitOfTime.DAY, Integer.MAX_VALUE);
+        assert period == null;
+
+        period = Period.createPeriodOrNull(UnitOfTime.WEEK, Integer.MAX_VALUE);
+        assert period == null;
+
+        period = Period.createPeriodOrNull(UnitOfTime.MONTH, Integer.MAX_VALUE);
+        assert period == null;
+    }
+
+
+    @Test
+    public void When_MakePeriodWithNotPositiveTimes_Expect_Return() {
+        Period period;
+
+        period = Period.createPeriodOrNull(UnitOfTime.HOUR, 0);
+        assert period == null;
+
+        period = Period.createPeriodOrNull(UnitOfTime.HOUR, -1);
+        assert period == null;
+
+        period = Period.createPeriodOrNull(UnitOfTime.HOUR, Integer.MIN_VALUE);
+        assert period == null;
+
+        period = Period.createPeriodOrNull(UnitOfTime.DAY, 0);
+        assert period == null;
+
+        period = Period.createPeriodOrNull(UnitOfTime.DAY, -1);
+        assert period == null;
+
+        period = Period.createPeriodOrNull(UnitOfTime.DAY, Integer.MIN_VALUE);
+        assert period == null;
+
+        period = Period.createPeriodOrNull(UnitOfTime.WEEK, 0);
+        assert period == null;
+
+        period = Period.createPeriodOrNull(UnitOfTime.WEEK, -1);
+        assert period == null;
+
+        period = Period.createPeriodOrNull(UnitOfTime.WEEK, Integer.MIN_VALUE);
+        assert period == null;
+
+        period = Period.createPeriodOrNull(UnitOfTime.MONTH, 0);
+        assert period == null;
+
+        period = Period.createPeriodOrNull(UnitOfTime.MONTH, -1);
+        assert period == null;
+
+        period = Period.createPeriodOrNull(UnitOfTime.MONTH, Integer.MIN_VALUE);
+        assert period == null;
+    }
+}
