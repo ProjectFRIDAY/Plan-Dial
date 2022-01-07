f1fa4a6b376114ddcb5054d12813d992a98d15a6
CaChiJ <chj7239@gmail.com>
2022-01-07 17:40:53 +0900

    feature : 사용자의 터치에 따라 회전하는 메인다이얼
    메인페이지의 메인다이얼을 추가함.
    메인다이얼이 사용자의 터치와 드래그에 따라 회전하는 기능을 추가함.

--- app/src/main/java/com/example/plandial/MainActivity.java
+++ app/src/main/java/com/example/plandial/MainActivity.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    SpinnableImageView spinnableImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinnableImageView = findViewById(R.id.main_dial);
     }
 }
