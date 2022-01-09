4e8ab0f1ac069a296bcffa75868b2948b6496ad7
Donghk0128 <donghk0128@gmail.com>
2022-01-09 21:14:49 +0900

    feat : 임박한 다이얼 표시 기능
    임박한 다이얼 기능 구현 코드 작성.

--- app/src/main/java/com/example/plandial/UrgentDials.java
+++ app/src/main/java/com/example/plandial/UrgentDials.java
@@ -1,4 +1,27 @@
 package com.example.plandial;
 
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+
 public class UrgentDials {
+    private static final ArrayList<Dial> UrgentDials = new ArrayList<>();
+    private static final ArrayList<Category> categories = new ArrayList<>();
+
+    private UrgentDials() {
+        //category, dial 불러옴.
+    }
+
+    public void getUrgentDials(){
+        for (Category category : categories) {
+            for(int i = 0; i < category.getDialCount(); ++i) {
+                OffsetDateTime startDateTime = category.getDialByIndex(i).getStartDateTime();
+                OffsetDateTime endDateTime = category.getDialByIndex(i).getEndDateTime();
+                long amount = startDateTime.until(endDateTime, ChronoUnit.SECONDS);
+                if(amount < 3600) {
+                    UrgentDials.add(category.getDialByIndex(i));
+                }
+            }
+        }
+    }
 }
