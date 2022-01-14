fe556d045794509c5dc19d9d6eeb4346c5633a5d
Mango-Juice <wjsalsrb5@naver.com>
2022-01-14 21:44:58 +0900

    style : 열린 중괄호 앞 띄어쓰기 적용
    열린 중괄호 앞에 띄어쓰기를 적용했습니다.

--- app/src/main/java/com/example/plandial/IconRecommendation.java
+++ app/src/main/java/com/example/plandial/IconRecommendation.java
@@ -33,36 +33,35 @@ public class IconRecommendation {
     public IconRecommendation(Context context) {
         // 최초 1회 -> 파일 준비
         if (!sIsReady) {
-            if(getIconVectors(context) && getWordVectors(context)){
+            if (getIconVectors(context) && getWordVectors(context)) {
                 sIsReady = true;
             }
         }
     }
 
-    private boolean getWordVectors(Context context){
+    private boolean getWordVectors(Context context) {
         // Word2Vec 모델 준비
-        try{
+        try {
             InputStream inputStream = context.getAssets().open("jsons/word_vectors.csv");
             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
             CSVReader read = new CSVReader(reader);
 
             String[] record = null;
             ArrayList<String> tmp;
-            while ((record = read.readNext()) != null){
-                 tmp = new ArrayList<>(Arrays.asList(Arrays.copyOfRange(record, 1, record.length)));
+            while ((record = read.readNext()) != null) {
+                tmp = new ArrayList<>(Arrays.asList(Arrays.copyOfRange(record, 1, record.length)));
                 sWordVectors.put(record[0], tmp);
             }
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
             return false;
         }
         return true;
     }
 
-    private boolean getIconVectors(Context context){
+    private boolean getIconVectors(Context context) {
         // 아이콘 벡터값 준비
-        try{
+        try {
             InputStream inputStream = context.getAssets().open("jsons/icon_vectors.json");
             int fileSize = inputStream.available();
 
@@ -74,81 +73,77 @@ public class IconRecommendation {
             JSONObject jsonObject = new JSONObject(json);
             JSONArray jsonArray = jsonObject.getJSONArray("data");
 
-            for(int i = 0; i < jsonArray.length(); i++)
-            {
+            for (int i = 0; i < jsonArray.length(); i++) {
                 JSONObject iconObject = jsonArray.getJSONObject(i);
                 sIconFileNames.add(iconObject.getString("icon") + ".png");
 
                 ArrayList<Double> vectorValues = new ArrayList<>();
                 JSONArray vectorArray = iconObject.getJSONArray("vector");
-                for(int j = 0; j < vectorArray.length(); j++)
-                {
+                for (int j = 0; j < vectorArray.length(); j++) {
                     Double value = vectorArray.getDouble(j);
                     vectorValues.add(value);
                 }
                 sIconVectors.add(vectorValues);
             }
 
-        }
-        catch (IOException | JSONException e) {
+        } catch (IOException | JSONException e) {
             e.printStackTrace();
             return false;
         }
         return true;
     }
 
-    private double dotOperation(final ArrayList<String> wordVector, final ArrayList<Double> iconVector){
+    private double dotOperation(final ArrayList<String> wordVector, final ArrayList<Double> iconVector) {
         // 내적
         double result = 0.0d;
-        for(int i = 0; i < sIconVectors.size(); i++) {
+        for (int i = 0; i < sIconVectors.size(); i++) {
             result += Double.parseDouble(wordVector.get(i)) * iconVector.get(i);
         }
         return result;
     }
 
-    private double normOperationString(final ArrayList<String> wordVector){
+    private double normOperationString(final ArrayList<String> wordVector) {
         // 노름
         double result = 0.0d;
-        for(int i = 0; i < sIconVectors.size(); i++) {
+        for (int i = 0; i < sIconVectors.size(); i++) {
             double value = Double.parseDouble(wordVector.get(i));
             result += value * value;
         }
         return result;
     }
 
-    private double normOperation(final ArrayList<Double> iconVector){
+    private double normOperation(final ArrayList<Double> iconVector) {
         // 노름
         double result = 0.0d;
-        for(int i = 0; i < sIconVectors.size(); i++) {
+        for (int i = 0; i < sIconVectors.size(); i++) {
             result += iconVector.get(i) * iconVector.get(i);
         }
         return result;
     }
 
-    public String getIconByName(String keyword){
-        try{
+    public String getIconByName(String keyword) {
+        try {
             ArrayList<String> wordVector = sWordVectors.get(keyword);
             int targetIndex = -1;
             double targetValue = -1;
 
-            for(int i = 0; i < sIconVectors.size(); i++)
-            {
+            for (int i = 0; i < sIconVectors.size(); i++) {
                 ArrayList<Double> iconVector = sIconVectors.get(i);
                 double value = dotOperation(wordVector, iconVector) / (Math.sqrt(normOperationString(wordVector)) * Math.sqrt(normOperation(iconVector)));
 
-                if(value > targetValue){
+                if (value > targetValue) {
                     targetValue = value;
                     targetIndex = i;
                 }
             }
             return sIconFileNames.get(targetIndex);
 
-        } catch(Exception e){
+        } catch (Exception e) {
             return "unknown.png";
         }
     }
 
-    public boolean getIsReady(){
+    public boolean getIsReady() {
         return sIsReady;
     }
 }
