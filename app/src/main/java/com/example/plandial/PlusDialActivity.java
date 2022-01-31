8ad9b8f018898352b557b7c4f333bf6ad79f3f38
CaChiJ <chj7239@gmail.com>
2022-01-31 12:48:30 +0900

    feat : 다이얼 생성 시 시작일 선택 기능 추가
    다이얼 생성 시에 다이얼로그를 이용해 시작일과 시간을 설정할 수 있게 되었습니다.

--- app/src/main/java/com/example/plandial/PlusDialActivity.java
+++ app/src/main/java/com/example/plandial/PlusDialActivity.java
@@ -1,5 +1,6 @@
 package com.example.plandial;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ImageButton;
 
@@ -8,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class PlusDialActivity extends AppCompatActivity {
     private ImageButton backButton;
+    private FormatTextView startDayInput;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,6 +20,15 @@ public class PlusDialActivity extends AppCompatActivity {
             // 뒤로가기 버튼 설정
             backButton = findViewById(R.id.BackButton);
             backButton.setOnClickListener(view -> finish());
+        }
+
+        {
+            startDayInput = findViewById(R.id.Input_Startday);
+            Activity activity = this;
+            startDayInput.setOnClickListener(view -> {
+                DateTimePickerDialog dateTimePickerDialog = new DateTimePickerDialog(activity, startDayInput);
+                dateTimePickerDialog.show();
+            });
         }
     }
 }
