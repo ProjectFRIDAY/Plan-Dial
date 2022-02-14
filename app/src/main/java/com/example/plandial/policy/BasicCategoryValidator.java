4ca3c8f51599bd5030cbaca9afcf8342f55de9fc
Mango-Juice <wjsalsrb5@naver.com>
2022-02-14 23:09:50 +0900

    feat : 카테고리 이름 중복 체크
    같은 이름의 카테고리를 생성할 수 없는 기능을 추가함.

--- app/src/main/java/com/example/plandial/policy/BasicCategoryValidator.java
+++ app/src/main/java/com/example/plandial/policy/BasicCategoryValidator.java
@@ -1,11 +1,14 @@
 package com.example.plandial.policy;
 
-public class BasicCategoryValidator implements ICategoryValidator {
-    public static final int NAME_MIN_LENGTH = 1;
-    public static final int NAME_MAX_LENGTH = 9;
+import com.example.plandial.DialManager;
 
+public class BasicCategoryValidator implements ICategoryValidator {
     @Override
     public boolean validateName(String name) {
         return name != null && NAME_MIN_LENGTH <= name.length() && name.length() <= NAME_MAX_LENGTH;
+    }
+
+    public boolean validateName(String name, DialManager dialManager) {
+        return dialManager.getCategoryByName(name) == null;
     }
 }
--- app/src/main/java/com/example/plandial/policy/ICategoryValidator.java
+++ app/src/main/java/com/example/plandial/policy/ICategoryValidator.java
@@ -1,5 +1,8 @@
 package com.example.plandial.policy;
 
 public interface ICategoryValidator {
+    int NAME_MIN_LENGTH = 1;
+    int NAME_MAX_LENGTH = 9;
+
     boolean validateName(String name);
 }
